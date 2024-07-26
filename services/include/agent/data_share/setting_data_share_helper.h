1ae611548dea888a9521ade93e3d2546b4b43bd1
15950533375 <wanghuan36@huawei.com>
2024-07-26 16:17:47 +0800

    fix boot complete in oobe
    Signed-off-by: woohoa <wanghuan36@huawei.com>

---
+++ services/include/agent/data_share/setting_data_share_helper.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef APP_DOMAIN_VERIFY_SETTINGDATASHAREHELPER_H
+#define APP_DOMAIN_VERIFY_SETTINGDATASHAREHELPER_H
+
+#include "datashare_helper.h"
+#include "datashare_predicates.h"
+#include "system_ability_definition.h"
+#include "singleton.h"
+#include "uri.h"
+
+namespace OHOS::AppDomainVerify {
+class SettingsDataShareHelper : public DelayedSingleton<SettingsDataShareHelper> {
+    DECLARE_DELAYED_SINGLETON(SettingsDataShareHelper);
+
+public:
+    static const std::string QUERY_SATELLITE_MODE_KEY;
+    static const std::string QUERY_SATELLITE_CONNECTED_KEY;
+    static const std::string SETTINGS_DATASHARE_URI;
+    int Query(Uri& uri, const std::string& key, std::string& values);
+
+private:
+    std::shared_ptr<DataShare::DataShareHelper> CreateDataShareHelper(int systemAbilityId);
+};
+}
+#endif  // APP_DOMAIN_VERIFY_SETTINGDATASHAREHELPER_H
