1df12635101f1cc6c6039371b615c1867dec04bf
15950533375 <wanghuan36@huawei.com>
2024-11-06 14:01:20 +0800

    use all userid to query ability
    Signed-off-by: woohoa <wanghuan36@huawei.com>

--- frameworks/common/src/bms/bundle_info_query.cpp
+++ frameworks/common/src/bms/bundle_info_query.cpp
@@ -131,7 +131,7 @@ bool BundleInfoQuery::GetBundleAbilityInfos(const std::string& bundleName, std::
     std::string identity = IPCSkeleton::ResetCallingIdentity();
     auto ret = bundleMgrProxy->GetBundleInfo(bundleName,
         AppExecFwk::BundleFlag::GET_BUNDLE_WITH_ABILITIES | AppExecFwk::BundleFlag::GET_BUNDLE_WITH_SKILL, bundleInfo,
-        AppExecFwk::Constants::START_USERID);
+        AppExecFwk::Constants::ALL_USERID);
     IPCSkeleton::SetCallingIdentity(identity);
     if (!ret) {
         APP_DOMAIN_VERIFY_HILOGE(APP_DOMAIN_VERIFY_MODULE_COMMON, "GetBundleInfo failed, ret: %{public}d.", ret);
